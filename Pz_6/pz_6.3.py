650186ba2b62d6f00df029375bf0f58638ddd424
K1eyt <kabelkov7.ru@gmail.com>
2024-12-18 22:21:44 +0300

    pz 6

--- Pz_6/pz_6.3.py
+++ Pz_6/pz_6.3.py
@@ -1,29 +1,31 @@
 #Дан список размера N, все элементы которого, кроме последнего, упорядочены повозрастанию. Сделать список упорядоченным, переместив последний элемент на новую позицию.
+
 import random
+
+def insert_last_element(sorted_list):
+    #Извлекаем последний элемент
+    last_element = sorted_list[-1]
+
+    #Перемещаем его влево, пока не найдем правильное место
+    i = len(sorted_list) - 2  # Начинаем с предпоследнего элемента
+    while i >= 0 and sorted_list[i] > last_element:
+        i -= 1
+
+    #Вставляем элемент на правильное место
+    sorted_list[i + 1:] = [last_element] + sorted_list[i + 1:-1]
+
+#Генерируем случайный список размера N
 while True:
     try:
         N = int(input('Введите размер списка: '))
         break
     except ValueError:
         print("Введите целое число!")
+sorted_list = sorted(random.sample(range(1, 100), N - 1))  #Генерируем N-1 уникальных случайных чисел и сортируем
+last_element = random.randint(1, 100)  # енерируем последний элемент
+sorted_list.append(last_element)  #Добавляем последний элемент
 
-# Генерация случайного списка
-numbers = random.sample(range(1, 100), N)
-print(f'Список перед упорядочиванием: {numbers}')
+print("Исходный список:", sorted_list)
+insert_last_element(sorted_list)
+print("Упорядоченный список:", sorted_list)
 
-# Вставка последнего элемента на правильную позицию
-last_element = numbers[-1]
-sorted_numbers = []
-
-#Строим новый отсортированный список
-for i in range(N - 1):
-    if numbers[i] <= last_element:
-        sorted_numbers.append(numbers[i])
-    else:
-        sorted_numbers.append(last_element)
-        last_element = numbers[i]
-
-#Добавляем оставшийся элемент, если он еще не добавлен
-sorted_numbers.append(last_element)
-
-print(f'Упорядоченный список: {sorted_numbers}')
